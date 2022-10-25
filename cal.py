9dec531c3d91b87ada105ac7a176ca0601cc092d
Angelica Aluo <angelicaaluo@Angelicas-MacBook-Air.local>
2022-10-25 23:23:12 +0100

    carry out basic math

---
+++ cal.py
@@ -0,0 +1,21 @@
+import math
+
+# numbers=[]
+def addition():
+    numbers=[]
+    while True:
+        user_input=int(input('>>: '))
+        numbers.append(user_input)
+        all_num=sum(numbers)
+        print(all_num)
+
+def subtract():
+    max_user_input =int(input('>>'))
+    while True:
+        max_user_input-=int(input('>'))
+        print(max_user_input)
+
+
+
+#addition()
+#subtract()
