523c34153343d5d50b8dab540be233af85ebb89f
izuutech <izutechsofficial@gmail.com>
2022-11-09 16:37:49 +0100

    add quadratic function

---
+++ quadratic.py
@@ -0,0 +1,19 @@
+import math
+
+def solve_quadratic(a, b, c):
+    count=0
+    answers=[]
+    while count<2:
+        if count==0:
+            x=(-b+(math.sqrt((b*b)-(4*a*c))))/(2*a)
+            answers.append(x)
+            count+=1
+        else:
+            x=(-b-(math.sqrt((b*b)-(4*a*c))))/(2*a)
+            answers.append(x)
+            print(answers)
+            break
+        
+solve_quadratic(2, 80, 6)
+    
+    
